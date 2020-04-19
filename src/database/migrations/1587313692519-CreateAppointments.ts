b427114720a3633c8559269c1a8d21658227e596
Fernando Binasco <fernando.binasco@gmail.com>
2020-04-19 15:20:14 -0300

    FIX final implementacao typeorm

--- src/database/migrations/1587313692519-CreateAppointments.ts
+++ src/database/migrations/1587313692519-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1587313692519
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
